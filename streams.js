0226a7d83ce7a6a8701726f2c51448bbade4a30d
drgx2x <40275060+drgx2x@users.noreply.github.com>
2026-09-01 01:01:50 +0700

    Update streams.js

--- streams.js
+++ streams.js
@@ -1,57 +1,27 @@
 const myStreams = [
-    { 
-        name: "MetroTV", 
-        url: "https://www.youtube.com/watch?v=hjNOqYN6UJo",
-        type: "youtube"
-    },
-    { 
-        name: "CNN Indonesia", 
-        url: "https://www.youtube.com/watch?v=PDDOkUq33Sw",
-        type: "youtube"
-    },
-    { 
-        name: "KOMPAS TV", 
-        url: "https://www.youtube.com/watch?v=DOOrIxw5xOw",
-        type: "youtube"
-    },
-    { 
-        name: "tvOne", 
-        url: "https://www.youtube.com/watch?v=rQJoEpzKkNk",
-        type: "youtube"
-    },
-    { 
-        name: "Official iNews", 
-        url: "https://www.youtube.com/watch?v=WHTPYAvakTg",
-        type: "youtube"
-    },
-    { 
-        name: "Sky News", 
-        url: "https://www.youtube.com/watch?v=xDWQ3LkccY8",
-        type: "youtube"
-    },
-    { 
-        name: "GB News", 
-        url: "https://www.youtube.com/watch?v=I8E6eDmhegY",
-        type: "youtube"
-    },
-    { 
-        name: "France 24 English", 
-        url: "https://www.youtube.com/watch?v=HvZt-nh9sGg",
-        type: "youtube"
-    },
-    { name: "Fox News", url: "https://m.youtube.com/watch?v=C96oohpWBGw", type: "youtube" },
-    { name: "CNN", url: "https://m.youtube.com/watch?v=GotlA1KKWoo", type: "youtube" },
-    { name: "ABC News", url: "https://m.youtube.com/watch?v=uUELoBTBuFw", type: "youtube" },
-    { name: "Bloomberg", url: "https://m.youtube.com/watch?v=QB5BNdBFujE", type: "youtube" },
-    { name: "Yahoo Finance", url: "https://m.youtube.com/watch?v=KQp-e_XQnDE", type: "youtube" },
-    { name: "Al Jazeera", url: "https://m.youtube.com/watch?v=gCNeDWCI0vo", type: "youtube" },
-    { name: "rifftrax", url: "https://m.twitch.tv/rifftrax", type: "twitch" },
-    { name: "Lofi Girl", url: "https://www.twitch.tv/lofigirl", type: "twitch" },
-    { name: "Leekbeats", url: "https://www.twitch.tv/leekbeats", type: "twitch" },
-    { name: "Yu-Gi-Oh!", url: "https://www.twitch.tv/yugioh_subs", type: "twitch" },
-    { name: "Police Chase", url: "https://www.twitch.tv/policechasenetwork247", type: "twitch" },
-    { name: "RetroCrush", url: "https://www.twitch.tv/retrocrush_tv", type: "twitch" },
-    { name: "FailArmy", url: "https://www.twitch.tv/failarmy", type: "twitch" },
-    { name: "The Asylum", url: "https://www.twitch.tv/theasylummovies", type: "twitch" },
-    { name: "Tokyo Street View", url: "https://www.twitch.tv/tokyostreetview", type: "twitch" }
-];
+    { "name": "KOMPAS TV", "url": "https://www.youtube.com/watch?v=DOOrIxw5xOw", "type": "youtube" },
+    { "name": "Official iNews", "url": "https://www.youtube.com/watch?v=WHTPYAvakTg", "type": "youtube" },
+    { "name": "CNN Indonesia", "url": "https://www.youtube.com/watch?v=PDDOkUq33Sw", "type": "youtube" },
+    { "name": "tvOne", "url": "https://www.youtube.com/watch?v=rQJoEpzKkNk", "type": "youtube" },
+    { "name": "MetroTV", "url": "https://www.youtube.com/watch?v=hjNOqYN6UJo", "type": "youtube" },
+    { "name": "Sky News", "url": "https://www.youtube.com/watch?v=xDWQ3LkccY8", "type": "youtube" },
+    { "name": "Al Jazeera", "url": "https://m.youtube.com/watch?v=gCNeDWCI0vo", "type": "youtube" },
+    { "name": "GB News", "url": "https://www.youtube.com/watch?v=I8E6eDmhegY", "type": "youtube" },
+    { "name": "France 24 English", "url": "https://www.youtube.com/watch?v=HvZt-nh9sGg", "type": "youtube" },
+    { "name": "Fox News", "url": "https://m.youtube.com/watch?v=C96oohpWBGw", "type": "youtube" },
+    { "name": "CNN", "url": "https://m.youtube.com/watch?v=GotlA1KKWoo", "type": "youtube" },
+    { "name": "ABC News", "url": "https://m.youtube.com/watch?v=uUELoBTBuFw", "type": "youtube" },
+    { "name": "Bloomberg", "url": "https://m.youtube.com/watch?v=QB5BNdBFujE", "type": "youtube" },
+    { "name": "Yahoo Finance", "url": "https://m.youtube.com/watch?v=KQp-e_XQnDE", "type": "youtube" },
+    { "name": "TalkTV", "url": "https://www.youtube.com/watch?v=DFYaNjzI1aI", "type": "youtube" },
+    { "name": "TalkSports", "url": "https://www.youtube.com/watch?v=uLWEssCrXYc", "type": "youtube" },
+    { "name": "Rifftrax", "url": "https://m.twitch.tv/rifftrax", "type": "twitch" },
+    { "name": "Lofi Girl", "url": "https://www.twitch.tv/lofigirl", "type": "twitch" },
+    { "name": "Leekbeats", "url": "https://www.twitch.tv/leekbeats", "type": "twitch" },
+    { "name": "Yu-Gi-Oh!", "url": "https://www.twitch.tv/yugioh_subs", "type": "twitch" },
+    { "name": "Police Chase", "url": "https://www.twitch.tv/policechasenetwork247", "type": "twitch" },
+    { "name": "RetroCrush", "url": "https://www.twitch.tv/retrocrush_tv", "type": "twitch" },
+    { "name": "FailArmy", "url": "https://www.twitch.tv/failarmy", "type": "twitch" },
+    { "name": "The Asylum", "url": "https://www.twitch.tv/theasylummovies", "type": "twitch" },
+    { "name": "Tokyo Street View", "url": "https://www.twitch.tv/tokyostreetview", "type": "twitch" }
+]
