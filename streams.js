8945d15db9a4251d492ceace667901d853e1bf87
drgx2x <40275060+drgx2x@users.noreply.github.com>
2026-09-05 11:56:42 +0700

    Add files via upload

--- streams.js
+++ streams.js
@@ -6,7 +6,7 @@ const myStreams = [
     },
     {
         "name": "Official iNews",
-        "url": "https://www.youtube.com/watch?v=WHTPYAvakTg",
+        "url": "https://m.youtube.com/watch?v=UMe1AKUyUN0",
         "type": "youtube"
     },
     {
